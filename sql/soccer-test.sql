1db4a02a0754b5ef957978d381daa000687742ff
hohyun0406 <hohyun0406@gmail.com>
2024-03-13 21:52:54 +0900

    study sql test 22.23

--- sql/soccer-test.sql
+++ sql/soccer-test.sql
@@ -88,7 +88,7 @@ WHERE `POSITION` = 'MF' AND height BETWEEN 170 AND 180
 -- 문제 7-- 수원을 연고지로 하는 골키퍼는 누구인가?
 SELECT *
 FROM player
-WHERE nation = '수원' AND POSITION = 'GK';
+WHERE team_id = 'K02' AND POSITION = 'GK';
 
 -- 문제 8-- 서울팀 선수들 이름, 키, 몸무게 목록으로 출력하시오-- 키와 몸무게가 없으면 "0" 으로 표시하시오-- 키와 몸무게는 내림차순으로 정렬하시오
 SELECT player_name, IFNULL(NULLIF(height,''), '0') AS height , IFNULL(NULLIF(weight,''), '0') AS weight
@@ -129,8 +129,9 @@ ORDER BY 1,2
 -- [팀 명]             [홈구장]
 -- 수원[ ]삼성블루윙즈 수원월드컵경기장
 
-SELECT t.region_name, t.team_name, s.stadium_name
-FROM stadium s JOIN team t ON t.stadium_id = s.stadium_id
+SELECT CONCAT (t.region_name,' ' , t.team_name) AS '팀명', s.stadium_name '홈구장'
+FROM stadium s
+JOIN team t ON s.stadium_id = t.stadium_id
 WHERE t.region_name LIKE '수원%' AND s.stadium_name LIKE '수원%';
 
 -- 문제 12
@@ -173,9 +174,12 @@ WHERE sc.sche_date = '20120317';
 -- 선수, 포지션,팀명 (연고지포함),
 -- 스타디움, 경기날짜를 구하시오
 -- 연고지와 팀이름은 간격을 띄우시오(수원[]삼성블루윙즈)
-SELECT *
-FROM
-WHERE
+SELECT p.player_name, p.position, CONCAT (t.region_name,' ' , t.team_name), s.stadium_name, sc.sche_date
+FROM stadium s
+    JOIN team t ON s.stadium_id = t.stadium_id
+    JOIN player p ON p.team_id = t.team_id
+    JOIN schedule sc ON s.stadium_id = sc.stadium_id
+WHERE sc.sche_date = '20120317' AND t.team_name = '스틸러스' AND p.`POSITION` = 'GK';
 
 
 -- 문제 16
@@ -183,7 +187,13 @@ WHERE
 -- 경기장 이름, 경기 일정
 -- 홈팀 이름과 원정팀 이름을
 -- 구하시오
-
+SELECT s.stadium_name AS '경기장이름', sc.sche_date AS '경기일정',
+        (SELECT t.team_name FROM team t WHERE t.team_id=sc.hometeam_id) AS '홈팀',
+        (SELECT t.team_name FROM team t WHERE t.team_id=sc.awayteam_id) AS '원정팀'
+FROM stadium s
+    JOIN team t ON s.stadium_id = t.stadium_id
+    JOIN schedule sc ON s.stadium_id = t.stadium_id
+WHERE sc.home_score - sc.away_score >=3 ;
 
 -- 문제 17
 -- STADIUM 에 등록된 운동장 중에서
@@ -191,10 +201,19 @@ WHERE
 -- 카운트 값은 19
 -- 힌트 : LEFT JOIN 사용해야함
 -- 경기장 이름, 홈 팀
+SELECT s.stadium_name, t.team_name
+FROM stadium s
+    LEFT JOIN team t ON s.stadium_id = t.stadium_id;
 
---문제 18 페이지네이션 로직을 위한
---플레이어 테이블에서 최상단 5개 로우를 출력
-SELECT * FROM player LIMIT 5;
+--위는 공부용 코드고 아래는 스칼라(서브쿼리)를 이용한 좋은 코드
+SELECT stadium_name, (SELECT t.team_name FROM team t WHERE t.stadium_id = st.stadium_id)
+FROM stadium st;
+
+
+-- 문제 18 페이지네이션 로직을 위한
+-- 플레이어 테이블에서 최상단 5개 로우를 출력
+SELECT player_name FROM player
+order BY 1 LIMIT 0, 5;
 
 -- 문제 19 (그룹바이: 집계함수 - 딱 5개 min, max, count, sum, avg)
 -- 평균키가 인천 유나이티스팀('K04')의 평균키  보다 작은 팀의
@@ -202,26 +221,56 @@ SELECT * FROM player LIMIT 5;
 -- 인천 유나이티스팀의 평균키 -- 176.59
 -- 키와 몸무게가 없는 칸은 0 값으로 처리한 후 평균값에
 -- 포함되지 않도록 하세요.
-SELECT *
-FROM team t
-    JOIN player p ON t.team_id = p.team_id
-WHERE
-GROUP BY
-HAVING AVG
 
+SELECT p.team_id, team_name, round(AVG(height),2)
+FROM team t
+	JOIN player p ON t.team_id = p.team_id
+WHERE if(p.height = '',0, p.height)
+GROUP BY p.team_id
+HAVING AVG(p.height) < (SELECT AVG(p2.height)
+							FROM team JOIN player p2 USING (team_id)
+							WHERE region_name ='인천');
 
 -- 문제 20
 -- 포지션이 MF 인 선수들의 소속팀명 및  선수명, 백넘버 출력
+SELECT t.team_name, p.player_name, p.back_no
+FROM team t
+    JOIN player p ON t.team_id = p.team_id
+WHERE p.POSITION = 'MF';
+
 
 -- 문제 21
 -- 가장 키큰 선수 5명 소속팀명 및  선수명, 백넘버 출력,
 -- 단 키  값이 없으면 제외
-
+SELECT t.team_name, p.player_name, p.back_no
+FROM team t
+    JOIN player p ON t.team_id = p.team_id
+ORDER BY p.height DESC
+LIMIT 5;
+--스칼라 이용해서 team이름 가져온 강사님 코드
+SELECT (SELECT t.team_name FROM team t WHERE t.team_id = p.team_id),player_name, back_no
+FROM player p
+ORDER BY height DESC LIMIT 5;
 
 
 -- 문제 22
 -- 선수 자신이 속한 팀의 평균키보다 작은  선수 정보 출력
 -- (select round(avg(height),2) from player)
+SELECT p.*
+FROM player p
+JOIN (SELECT p2.team_id, ROUND(AVG(p2.height), 2) avg
+		FROM player p2
+		WHERE p2.height != ''
+		GROUP BY p2.team_id) t_avg USING (team_id)
+WHERE p.height != '' AND p.height < AVG;
+
+--내일 다시 시도해보기
+
 
 -- 문제 23
 -- 2012년 5월 한달간 경기가 있는 경기장  조회
+SELECT s.stadium_name
+FROM stadium s
+    JOIN schedule sc ON s.stadium_id = sc.stadium_id
+WHERE sche_date LIKE '201205%';
+--join 사용하지 않고, schedule 테이블이랑 스칼라 서브 쿼리 써서 해보기.
